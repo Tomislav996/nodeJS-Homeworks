ca3f3015be364e4a515a430ff210ba8661638b91
Tomislav996 <trpeskitomislav@outlook.com>
2023-03-05 18:20:53 +0100

    .

---
+++ homework_4_events/My_modules/exports.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+import fs from "fs";
+
+export let studentsArray = [];
+
+
+function Student(fullname,email,password){
+    this.id = uuidv4(),
+    this.fullname = fullname,
+    this.email = email,
+    this.password = password
+}
+
+export let appendStudentFullname = (studentFullname) =>{
+    fs.appendFileSync("greeting_log.txt",`\n${studentFullname}`);
+}
+
+
+export let generateStudentFunction = (fullname,email,password) => {
+    let newStudent = new Student(fullname, email, password)
+    studentsArray.push(newStudent);
+}
+
+export let saveStudentToJSON = students => {
+    fs.writeFileSync("students.json", JSON.stringify(students));
+}
+
+
---
+++ homework_4_events/homework_4.js
@@ -0,0 +1,19 @@
+import {EventEmitter} from "events";
+import {studentsArray, appendStudentFullname,generateStudentFunction,saveStudentToJSON } from "./My_modules/exports.js";
+const eventEmitter = new EventEmitter();
+
+
+
+eventEmitter.on("greet", (fullname,email,password) =>{
+    generateStudentFunction(fullname,email,password);
+    console.log(`Hello ${fullname}`);
+    appendStudentFullname(fullname)
+    saveStudentToJSON(studentsArray);
+})
+
+
+
+eventEmitter.emit("greet","Jane Smith", "jane@gmail.com", "dfgjdfg");
+eventEmitter.emit("greet","John Smith", "john@gmail.com", "123456");
+eventEmitter.emit("greet","Bob Bobsky", "bobbobsky@outlook.com", "qwerty");
+eventEmitter.emit("greet","Paul Anderson", "paul@gmail.com", "irncvnj");
